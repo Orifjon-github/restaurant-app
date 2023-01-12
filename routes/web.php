837999aede2ef3ff55a7a9955dca632f6a919f9b
Orifjon94 <avazbekoripov94@gmail.com>
2023-01-12 20:59:15 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\MenuController::class, 'index']);
+
+ Route::prefix('admin')->group(function () {
+     Route::get('/', function () {
+         return redirect('admin/login');
+     });
+     Route::get('/login', [AuthController::class, 'login'])->name('login');
+     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+     Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
+//     Route::get('/register', [AuthController::class, 'register'])->name('register');
+//     Route::post('/register', [AuthController::class, 'register_store'])->name('register_store');
+     Route::view('/dashboard', 'admin.index');
+     Route::resource('/categories', \App\Http\Controllers\CategoryController::class);
+ });
+
